95861497714ac5881d8c5636e8882e6a5d4ad49c
Goutam Nath <nathgoutam93@gmail.com>
2022-02-27 16:31:20 +0530

    Bug Fix

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,14 @@ function Header() {
       <h1 className="text-2xl font-nunito flex font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-400 via-fuchsia-400 to-blue-400">
         Link.gen
       </h1>
+      <a
+        rel="noreferrer"
+        href="https://linkpile-bffd7.web.app/"
+        target="_blank"
+        className="px-4 py-2 text-white font-nunito font-bold rounded-3xl bg-primary-accent hover:bg-secondary-accent"
+      >
+        Try Linkpile
+      </a>
     </header>
   );
 }
--- src/reducers/adminReducer.js
+++ src/reducers/adminReducer.js
@@ -26,6 +26,9 @@ const initialState = {
     linkedin: "",
     github: "",
     hashnode: "",
+    devto: "",
+    medium: "",
+    whatsapp: "",
   },
 };
 
